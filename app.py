4afdb5f7c39bacccc734e72873ca7931dcfa7b74
patrikSK <husnajpatrik@gmail.com>
2021-12-28 22:45:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,51 +8,128 @@ import mysql.connector as MYSQL
 app = Flask(__name__)
 CORS(app)
 
-orders = [
-          {
-          "car_type": "sedan",
-          "car_brand": "skoda",
-          "car_model": "fabia",
-          "problem": "auto nestartuje",
-          "first_name": "Amanda",
-          "last_name": "Green",
-          "tel_number": "0912345678",
-          "email": "green@gmail.com"     
-          },
-          {
-          "car_type": "hatchbag",
-          "car_brand": "volkswagen",
-          "car_model": "passat",
-          "problem": "nejdu stierace",
-          "first_name": "feri",
-          "last_name": "zhrabovca",
-          "tel_number": "09128521",
-          "email": "feri@gmail.com"     
-          }
-          ]
 
-@app.route("/", methods=["GET"])
+@app.route('/getOrders', methods=['GET'])			
 def main():
-  return jsonify(orders),200
+  myDb = MYSQL.connect(host="147.232.40.14", user="mk257st", passwd="Zeayohk7", database="mk257st")
+  cursor = myDb.cursor()
+  with open("ddl/car/selectAllCarOwner.ddl") as ddl_file:
+    query = ddl_file.read()
+  cursor.execute(query)
+  result = cursor.fetchall()
+  cursor.close()
+  myDb.close()
+  vysledok = []
+  for i in result:
+    vysledok.append('{'+'"car_id":"{}","owner_id":"{}","date":"{}","type":"{}","brand":"{}","model":"{}","problem":"{}","name":"{}","surname":"{}","phone":"{}","email":"{}"'.format(i[0],i[6],i[1],i[2],i[3],i[4],i[5],i[8],i[9],i[10],i[11])+'}')
+  vys = []
+  for k in range(len(vysledok)):
+    vys.append(eval(vysledok[k]))
+  return jsonify(vys),200
 
-@app.route("/vytvorit", methods=["POST"])
+
+
+
+
+
+@app.route("/createOrder", methods=["POST"])
 def create():
-    data = request.get_json(force=True)
-    data_dict = dict(data)
-    orders.append(data_dict)
-    return jsonify("created"),201
+  data = request.get_json(force=True)
+  data_dict = dict(data)
+  myDb = MYSQL.connect(host="147.232.40.14", user="mk257st", passwd="Zeayohk7", database="mk257st")
 
-@app.route("/upravit/<id>", methods=["PUT"])
+  cursor = myDb.cursor()
+  with open("ddl/owner/selectIdPhone.ddl") as ddl_file:
+    query = ddl_file.read()
+  cursor.execute(query)
+  result = cursor.fetchall()
+  cursor.close()
+  orders = []
+  for i in result:
+  	orders.append('{'+'"id":{},"phone":"{}"'.format(i[0],i[1])+'}')
+  owners = []
+  for i in range(len(orders)):
+  	owners.append(eval(orders[i]))
+  for i in owners:
+    if i["phone"] == data_dict["phone"]:
+      cursor = myDb.cursor()
+      with open("ddl/car/insert.ddl") as ddl_file:
+        query = ddl_file.read()
+      query = query.format(data_dict['type'],data_dict['brand'],data_dict['model'],data_dict['problem'],i['id'])
+      cursor.execute(query)
+      cursor.close()
+      break
+  else:
+    id = 1
+    for j in owners:
+      if id == j["id"]:
+        id = j["id"] + 1
+    cursor = myDb.cursor()
+    with open("ddl/owner/insert.ddl") as ddl_file:
+        query = ddl_file.read()
+    query = query.format(id,data_dict['name'],data_dict['surname'],data_dict['phone'],data_dict['email'])
+    cursor.execute(query)
+    myDb.commit()
+    cursor.close()
+
+    cursor = myDb.cursor()
+    with open("ddl/car/insert.ddl") as ddl_file:
+        query = ddl_file.read()
+    query = query.format(data_dict['type'],data_dict['brand'],data_dict['model'],data_dict['problem'],id)
+    cursor.execute(query)
+    cursor.close()
+
+
+  myDb.commit()
+  myDb.close()
+  return jsonify("created"),201
+
+
+
+
+
+
+@app.route("/updateOrder/<id>", methods=["PUT"])
 def update(id):
-    data = request.get_json(force=True)
-    data_dict = dict(data)
-    ovocie[int(id)] = data_dict["upravit"]
-    return jsonify("updated"),201
+  data = request.get_json(force=True)
+  data_dict = dict(data)
+  myDb = MYSQL.connect(host="147.232.40.14", user="mk257st", passwd="Zeayohk7", database="mk257st")
 
-@app.route("/vymazat/<id>", methods=["DELETE"])
+  cursor = myDb.cursor()
+  with open("ddl/owner/update.ddl") as ddl_file:
+    query = ddl_file.read()
+  query = query.format(data_dict["owner_id"],data_dict['name'],data_dict['surname'],data_dict['phone'],data_dict['email'],data_dict['owner_id'])
+  cursor.execute(query)
+  myDb.commit()
+  cursor.close()
+
+  cursor = myDb.cursor()
+  with open("ddl/car/update.ddl") as ddl_file:
+    query = ddl_file.read()
+  query = query.format(id,data_dict['type'],data_dict['brand'],data_dict['model'],data_dict['problem'],data_dict['owner_id'],id)
+  cursor.execute(query)
+  myDb.commit()
+  cursor.close()
+  
+  myDb.close()
+  return jsonify("Updated"),200
+
+
+
+
+
+@app.route("/deleteOrder/<id>", methods=["DELETE"])
 def delete(id):
-    del orders[int(id)]
-    return jsonify("deleted"),204
+  myDb = MYSQL.connect(host="147.232.40.14", user="mk257st", passwd="Zeayohk7", database="mk257st")
+  cursor = myDb.cursor()
+  with open("ddl/car/deleteId.ddl") as ddl_file:
+    query = ddl_file.read()
+  query = query.format(id)
+  cursor.execute(query)
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("delete"),204
 
-if __name__ == "__main__":
-    app.run()
+
+app.run()
